079d9be0a72e73721bed5ce3c963fdc4b798b6ce
Leandro Kayque Santos Ximenes <52089912847@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-05-18 10:31:24 -0300

    Aula 18 FINAL

--- aula18/produto/adicionar_produto.php
+++ aula18/produto/adicionar_produto.php
@@ -1,29 +1,29 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Cadastro de Produto</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de Produto</title>
 </head>
 <body>
     <h1>Cadastro de Produto</h1>
     <hr>
-    <form action="inserir.php" method="post">
+    <form action="inserir.php" method="post" enctype="multipart/form-data">
+    <label for="nome">Nome:</label><br>
+    <input type="text" name="nome" id="nome"><br>
 
-        <label for="nome">Nome:</label><br>
-        <input type="text" name="nome" id="nome"><br>
+    <label for="descricao">Descrição:</label><br>
+    <textarea name="descricao" id="descricao"></textarea><br>
 
-        <label for="descricao">Descrição:</label><br>
-        <textarea name="descricao" id="descricao"></textarea><br>
+    <label for="preco">Preço:</label><br>
+    <input type="number" step="0.01" name="preco" id="preco"><br>
 
-        <label for="preco">Preço:</label><br>
-        <input type="number" step="0.01" name="preco" id="preco"><br>
-
-        <label for="foto">Foto:</label><br>
-        <input type="file" name="foto" id="foto"><br>
-        
-        <button type="submit">Cadastrar</button>
-        
-    </form>
+    <label for="foto">Foto:</label><br>
+    <input type="file" name="foto" id="foto"><br>
+    
+    <button type="submit">Cadastrar</button>
+    
+</form>
 </body>
 </html>
+```
--- aula18/produto/listar_produtos.php
+++ aula18/produto/listar_produtos.php
@@ -1,17 +1,57 @@
-<?php
-require_once "../conexao.php";
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Listagem de Produtos</title>
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
 
-$sql = "SELECT * FROM produto";
-$resultado = $conexao->query($sql);
+        th, td {
+            padding: 8px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
 
-if ($resultado->num_rows > 0) {
-    echo "<table>";
-    echo "<tr><th>ID</th><th>Nome</th><th>Descrição</th><th>Preço</th></tr>";
-    while ($linha = $resultado->fetch_assoc()) {
-        echo "<tr><td>".$linha['idproduto']."</td><td>".$linha['nome']."</td><td>".$linha['descricao']."</td><td>".$linha['preco']."</td></tr>";
+        th {
+            background-color: #f2f2f2;
+        }
+
+        img {
+            max-width: 100px;
+            max-height: 100px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Listagem de Produtos</h1>
+    <?php
+    require_once "../conexao.php";
+
+    $sql = "SELECT * FROM produto";
+    $resultado = $conexao->query($sql);
+
+    if ($resultado->num_rows > 0) {
+        echo "<table>";
+        echo "<tr><th>ID</th><th>Nome</th><th>Descrição</th><th>Preço</th><th>Foto</th></tr>";
+        while ($linha = $resultado->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>".$linha['idproduto']."</td>";
+            echo "<td>".$linha['nome']."</td>";
+            echo "<td>".$linha['descricao']."</td>";
+            echo "<td>".$linha['preco']."</td>";
+
+            $foto = "../uploads/".$linha['foto'];
+            echo "<td><img src='".$foto."' alt='Foto do produto'></td>";
+
+            echo "</tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "Nenhum produto encontrado.";
     }
-    echo "</table>";
-} else {
-    echo "Nenhum produto encontrado.";
-}
-?>
+    ?>
+</body>
+</html>
